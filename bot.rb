e0d97bf247c8233b83cba277dca8bd79b083d430
Ciel248 <inoue.yusuke.928+ciel248@gmail.com>
2016-04-09 00:01:24 +0900

    unicornの設定を追加

--- bot.rb
+++ bot.rb
@@ -11,38 +11,44 @@ LINEBOT_API_HOST = "https://trialbot-api.line.me"
 TO_CHANNEL = "1383378250" # FIXED_VALUE
 EVENT_TYPE = "138311608800106203" # FIXED_VALUE
 
-helpers do
-  def post_text(to, text)
-    content = {
-      contentType: 1,
-      toType: 1,
-      text: text
-    }
+class BotApp < Sinatra::Base
+  helpers do
+    def post_text(to, text)
+      content = {
+        contentType: 1,
+        toType: 1,
+        text: text
+      }
 
-    post_event(to, content)
+      post_event(to, content)
+    end
+
+    def post_event(to, content)
+      body = {
+        to: [to],
+        toChannel: TO_CHANNEL,
+        eventType: EVENT_TYPE,
+        content: content
+      }.to_json
+
+      conn = Faraday.new LINEBOT_API, ssl: true
+      conn.post do |req|
+        req.url "/v1/events"
+        req.headers["Content-type"]                 = "application/json; charset=UTF-8"
+        req.headers["X-Line-ChannelID"]             = Settings.linebot.channel_id,
+        req.headers["X-Line-ChannelSecret"]         = Settings.linebot.channel_secret,
+        req.headers["X-Line-Trusted-User-With-ACL"] = Settings.linebot.mid,
+        req.body = body
+      end
+    end
   end
 
-  def post_event(to, content)
-    body = {
-      to: [to],
-      toChannel: TO_CHANNEL,
-      eventType: EVENT_TYPE,
-      content: content
-    }.to_json
-
-    conn = Faraday.new LINEBOT_API, ssl: true
-    conn.post do |req|
-      req.url "/v1/events"
-      req.headers["Content-type"]                 = "application/json; charset=UTF-8"
-      req.headers["X-Line-ChannelID"]             = Settings.linebot.channel_id,
-      req.headers["X-Line-ChannelSecret"]         = Settings.linebot.channel_secret,
-      req.headers["X-Line-Trusted-User-With-ACL"] = Settings.linebot.mid,
-      req.body = body
-    end
+  post "/callback" do
+    params = JSON.parse request.body.read
+    post_text(params["result"]["content"]["from"], "hello")
   end
 end
 
-post "/callback" do
-  params = JSON.parse request.body.read
-  post_text(params["result"]["content"]["from"], "hello")
+if $0 == __FILE__
+  BotApp.run
 end
---
+++ config/unicorn/local.rb
@@ -0,0 +1,24 @@
+@app_path = File.join(File.dirname(__FILE__), "..", "..")
+working_directory @app_path
+
+worker_processes 2
+preload_app true
+timeout 60
+listen 3000
+
+stdout_path "#{@app_path}/log/unicorn.log"
+stderr_path "#{@app_path}/log/unicorn.err"
+
+before_fork do |server, worker|
+  ENV["BUNDLE_GEMFILE"] = File.expend_path("Gemfile", ENV["RAILS_ROOT"])
+end
+
+before_fork do |server, worker|
+  old_pid = "#{server.config[:pid]}.oldbin"
+  if File.exists?(old_pid) && server.pid != old_pid
+    begin
+      Process.kill("QUIT", File.read(old_pid).to_i)
+    rescue Erron::ENOENT, Erron::ESRCH
+    end
+  end
+end
